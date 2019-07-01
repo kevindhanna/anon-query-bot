164966f35590ed0d6b689217b713e4575347d7d0
kevindhanna <kevindhanna@gmail.com>
2019-07-01 21:00:19 +0100

    Added default values for CHANNEL_NAME and DM_RESPONSE env variables

--- slack-anon-query/bot.rb
+++ slack-anon-query/bot.rb
@@ -4,10 +4,18 @@ module SlackAnonQuery
       return unless direct_message?(data)
       begin
         webclient = Slack::Web::Client.new token: ENV["SLACK_API_TOKEN"]
-        channelid = webclient.channels_info(channel: ENV["CHANNEL_NAME"])['channel']['id']
+        if ENV["CHANNEL_NAME"].nil?
+          channelid = webclient.channels_info(channel: "#anonymoose-questions")['channel']['id']
+        else
+          channelid = webclient.channels_info(channel: ENV["CHANNEL_NAME"])['channel']['id']       
+        end
         client.typing(channel: data.channel)
         client.say(channel: channelid, text: data.text)
-        client.say(channel: data.channel, text: ENV["DM_RESPONSE"]) 
+        if ENV["DM_RESPONSE"].nil?
+          client.say(channel: data.channel, text: "Thanks, I've submitted your question.")
+        else
+          client.say(channel: data.channel, text: ENV["DM_RESPONSE"]) 
+        end
       rescue => exception
         return client.say(channel: data.channel, text: "Sorry, something went wrong, please contact your administrator.")
       end
