54089b80c367ee473b531a0f350a23b77ef7feaa
kevindhanna <kevindhanna@gmail.com>
2019-07-16 11:49:46 +0100

    workaround for heroku sleep double-post issue

--- slack-anon-query/bot.rb
+++ slack-anon-query/bot.rb
@@ -1,7 +1,10 @@
 module SlackAnonQuery
   class Bot < SlackRubyBot::Bot
+    last_message = ""
     match /.*/ do |client, data, _match|
       return unless direct_message?(data)
+      # work around for heroku sleep causing double posts
+      return unless data.text != last_message
       begin
         webclient = Slack::Web::Client.new token: ENV["SLACK_API_TOKEN"]
         if ENV["CHANNEL_NAME"].nil?
@@ -11,12 +14,14 @@ module SlackAnonQuery
         end
         client.typing(channel: data.channel)
         client.say(channel: channelid, text: data.text)
+        last_message = data.text
         if ENV["DM_RESPONSE"].nil?
           client.say(channel: data.channel, text: "Thanks, I've submitted your question.")
         else
           client.say(channel: data.channel, text: ENV["DM_RESPONSE"]) 
         end
-      rescue => exception
+      rescue Exception => e
+        puts e.message
         return client.say(channel: data.channel, text: "Sorry, something went wrong, please contact your administrator.")
       end
     end
