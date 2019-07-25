e63bc9d71635cbe6c544cbf633f2a93e84c42220
kevindhanna <kevindhanna@gmail.com>
2019-07-25 19:58:29 +0100

    logging fixed

--- app/slack_anon_query/bot.rb
+++ app/slack_anon_query/bot.rb
@@ -7,25 +7,26 @@ Slack.configure do |config|
 end
 
 $bot = Slack::RealTime::Client.new
-log.info ("initialize") {"Initializing..."}
+log.info ("slackbot - initialize") {"Initializing..."}
 
 $bot.on :message do |data|
   if data.channel && data.channel[0] == "D"
-    log.info ("DM recieved") {"DM recieved, processing"}
+    log.info ("slackbot - DM recieved") {"DM recieved on channelid #{data.channel}, processing"}
     begin
       if ENV["CHANNEL_NAME"].nil?
         channelid =  $bot.channels.values.select{ |channel| channel.name == "anonymoose-questions" }[0].id
       else
         channelid =  $bot.channels.values.select{ |channel| channel.name == ENV["CHANNEL_NAME"] }[0].id
       end
-      log.info ("set channelid") {"Set message output channel to #{channelid}"}
+      log.info ("slackbot - set channelid") {"Set message output channel to #{channelid}"}
       last_message = REDIS.get('lm')
-      log.info ("check last message") { "Checking for duplicate messages" }
+      log.info ("slackbot - check last message") { "Compaing to last sent message to minimise duplicate messages..." }
       if last_message != data.text  
-        log.info ("post message") { "Posting message to channel" }
+        log.info ("slackbot - post message") { "Posting message to channel - '#{data.text}'" }
         $bot.message(channel: channelid, text: data.text)
         REDIS.set('lm', data.text)
         REDIS.expire('lm', 30)
+        log.info ("slackbot - inform user") { "Replying to user their DM channel"}
         if ENV["DM_RESPONSE"].nil?
           $bot.message(channel: data.channel, text: "Thanks, I've submitted your question.")
         else
@@ -34,7 +35,7 @@ $bot.on :message do |data|
       end
     rescue Exception => e
       puts e.message
-      log.info ("ERROR") { "#{e}" }
+      log.info ("slackbot - ERROR") { "#{e}" }
       $bot.message(channel: data.channel, text: "Sorry, something went wrong, please contact your administrator.")
     end
   end
